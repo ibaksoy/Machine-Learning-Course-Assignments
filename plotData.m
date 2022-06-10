a5ec511f7acc50af05565877bea5ebf6007d46c4
ibaksoy <95378430+ibaksoy@users.noreply.github.com>
2022-06-09 22:57:23 -0500

    Update plotData.m

--- plotData.m
+++ plotData.m
@@ -5,15 +5,6 @@ function plotData(x, y)
 
 figure; % open a new figure window
 
-data = load('ex1data1.txt');
-X = data(:, 1);
-y = data(:, 2);
-m = length(y);
-
-plot(X, y, 'rx', 'MarkerSize', 10);
-xlabel('Profit in $10,000s');
-ylabel('Population of City in 10,000s');
-
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Plot the training data into a figure using the
@@ -26,7 +17,16 @@ ylabel('Population of City in 10,000s');
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
+% the solution for this exercise
 
+data = load('ex1data1.txt');
+X = data(:, 1);
+y = data(:, 2);
+m = length(y);
+
+plot(X, y, 'rx', 'MarkerSize', 10);
+xlabel('Profit in $10,000s');
+ylabel('Population of City in 10,000s');
 
 
 
